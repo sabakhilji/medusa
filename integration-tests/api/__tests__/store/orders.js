8e5237706e56825841df29b6f290c78ad05c1dcc
Sebastian Rindom <skrindom@gmail.com>
2022-07-15 12:57:59 +0200

    cleanup

--- integration-tests/api/__tests__/store/orders.js
+++ integration-tests/api/__tests__/store/orders.js
@@ -6,12 +6,8 @@ const {
   ShippingProfile,
   Product,
   ProductVariant,
-  MoneyAmount,
   LineItem,
   Payment,
-  Cart,
-  ShippingMethod,
-  Swap,
 } = require("@medusajs/medusa")
 
 const setupServer = require("../../../helpers/setup-server")
@@ -28,7 +24,7 @@ describe("/store/carts", () => {
   beforeAll(async () => {
     const cwd = path.resolve(path.join(__dirname, "..", ".."))
     dbConnection = await initDb({ cwd })
-    medusaProcess = await setupServer({ cwd, verbose: true })
+    medusaProcess = await setupServer({ cwd, verbose: false })
   })
 
   afterAll(async () => {
